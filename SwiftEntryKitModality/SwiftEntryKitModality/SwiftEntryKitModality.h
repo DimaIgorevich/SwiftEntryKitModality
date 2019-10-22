ea1dd4afc6c1e4960e8a3a2902bf80c46fb4dd8a
dima <dzhimasan@gmail.com>
2019-10-22 16:20:43 +0300

    Imported project

---
+++ SwiftEntryKitModality/SwiftEntryKitModality/SwiftEntryKitModality.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEntryKitModality.h
+//  SwiftEntryKitModality
+//
+//  Created by dima on 10/22/19.
+//  Copyright © 2019 QuilCode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftEntryKitModality.
+FOUNDATION_EXPORT double SwiftEntryKitModalityVersionNumber;
+
+//! Project version string for SwiftEntryKitModality.
+FOUNDATION_EXPORT const unsigned char SwiftEntryKitModalityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEntryKitModality/PublicHeader.h>
+
+
